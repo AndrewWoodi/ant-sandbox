c7f6e2e4baad742e98b2d6c6310001ae0eb3e695
Andrew Woodyatt <andrew.woodyatt@commnia.com>
2022-05-05 16:59:23 +1000

    Add unit test to forbidden name validator.

--- src/app/pages/hero/validation/forbidden-name-validation.directive.spec.ts
+++ src/app/pages/hero/validation/forbidden-name-validation.directive.spec.ts
@@ -1,3 +1,4 @@
+import { FormControl } from '@angular/forms';
 import { ForbiddenNameValidationDirective } from './forbidden-name-validation.directive';
 
 describe('ForbiddenNameValidationDirective', () => {
@@ -5,4 +6,19 @@ describe('ForbiddenNameValidationDirective', () => {
     const directive = new ForbiddenNameValidationDirective();
     expect(directive).toBeTruthy();
   });
+
+  it('should raise an error for star wars', () => {
+    const starWars = 'star wars'
+    const control = new FormControl(starWars);
+    const directive = new ForbiddenNameValidationDirective();
+    expect(directive.validate(control)).toEqual({ 'forbiddenName': { 'value': starWars } });
+  });
+
+  it('should not raise an error for star trek', () => {
+    const starTrek = 'star trek'
+    const control = new FormControl(starTrek);
+    const directive = new ForbiddenNameValidationDirective();
+    expect(directive.validate(control)).toBeNull();
+  });
+
 });
